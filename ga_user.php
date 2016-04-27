4019996dfd9a779b2eff10c0d2720e4fc00e5eb4
swmati11 <swmati11@gmail.com>
2016-04-27 20:22:01 +0200

    Update ga_user.php

--- ga_user.php
+++ ga_user.php
@@ -3,9 +3,9 @@ require './core/main-core.php';
 try {
   $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
   $user_info = $connection->query('SELECT * FROM uzytkownicy WHERE id=$_GET["id"]');
-  echo $user_info['pass'];
+  echo $user_info['nick'];
 } catch (Exception $e) {
-  echo 'Wystąpił błąd z serwerem w lini'.__LINE__.' pliku '.__FILE__.'!';
+  echo 'Error at line'.__LINE__.' file '.__FILE__.'!';
 }
 
 ?>
